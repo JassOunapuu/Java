8bceb465192551cd1663776ecd0150a7b59a8abc
JassOunapuu <98941717+JassOunapuu@users.noreply.github.com>
2022-10-05 11:33:00 +0300

    Add files via upload

--- H6.java
+++ H6.java
@@ -5,27 +5,27 @@ import java.io.PrintWriter;
 import java.io.*;
 public class H6 {
 
-	public static void main(String[] args) {
+public static void main(String[] args) {
 		
-		ArrayList<Integer> list = new ArrayList <Integer>();
-		Scanner scanner = new Scanner (System.in);
+	ArrayList<Integer> list = new ArrayList <Integer>();
+	Scanner scanner = new Scanner (System.in);
 		
-		int k = 1;
+	int k = 1;
 		
-		while(k == 1) {
-			System.out.print("Sisestage arv: ");
-			String nr = scanner.nextLine();
-			
-			if(nr == "") {
-				break;
-			}
+	while(k == 1) {
+		System.out.print("Sisestage arv: ");
+		String nr = scanner.nextLine();
+		
+		if(nr == "") {
+			break;
+		}
 		
 			int number = Integer.parseInt(nr);
 			list.add(number);
-		}
-		int kokku = list.size();
-		int sum = list.stream().mapToInt(Integer::intValue).sum();
-		int keskmine = sum/kokku;
+			}
+			int kokku = list.size();
+			int sum = list.stream().mapToInt(Integer::intValue).sum();
+			int keskmine = sum/kokku;
 		
 
 		try {
@@ -34,7 +34,7 @@ public class H6 {
 			bw.println("Summa: "+sum);
 			bw.println("Arvude keskmine: "+ keskmine);
 			bw.close();
-			System.out.println("Fail loodud");
+			System.out.println("Fail tehtud");
 		} catch (IOException e) {
 			e.printStackTrace();
 		}
--- H7.java
+++ H7.java
@@ -5,14 +5,27 @@ public class H7 {
 
 	public static void main(String[] args) {
 		
-		Scanner scanner = new Scanner (System.in); //kasutaja sisestus
-		System.out.print("Esimene arv: ");
-		int n1 = scanner.nextInt();
-		System.out.print("Teine arv: ");
-		int n2 = scanner.nextInt();
+		viga();
+		}
+		
+	public static void viga() {
+		
+		Scanner scanner = new Scanner (System.in); 
 	
-	
-	
+	try {
+		System.out.println("Sisesta arv: ");
+		int arv1 = scanner.nextInt();
+		System.out.println("Sisesta arv: " );
+		int arv2 = scanner.nextInt();
+		
+		int s = arv1/arv2;
+		System.out.println(s);
+	} catch (Exception e) { 
+		System.out.println("error");
 	}
-
+	System.out.println("Programm töötab edasi");
+	viga();
+			   
+	}	 
+			
 }
